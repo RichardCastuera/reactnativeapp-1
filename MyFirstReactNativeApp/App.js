9c82df02a4a5cbfe098ffc3d7609986473a2b22b
Richard <richardcastuera16@gmail.com>
2024-06-05 21:12:28 +0800

    feat(project): Handled events and managed the list of goals

--- MyFirstReactNativeApp/App.js
+++ MyFirstReactNativeApp/App.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
+
   return (
     <View style={styles.appContainer}>
+      <View>
+        <Text style={styles.styleTitle}>My Goals</Text>
+      </View>
       <View style={styles.userGoals}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button style={styles.styleButton} title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.userAddedGoals}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
@@ -16,28 +40,30 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     paddingHorizontal: 20,
     paddingVertical: 50,
   },
+  styleTitle: {
+    fontSize: 34,
+    fontWeight: "bold",
+  },
   userGoals: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "#cccccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccccc",
-    flex: 1,
+    flex: 0.95,
     padding: 8,
-  },
-  styleButton: {
-    padding: 8,
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "green",
-    color: "green",
   },
   userAddedGoals: {
+    flex: 4,
     marginVertical: 20,
   },
 });

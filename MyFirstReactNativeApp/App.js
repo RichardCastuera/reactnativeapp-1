f41fec1bd68bfc02c2590384af5a9c18bf152cef
Richard <richardcastuera16@gmail.com>
2024-06-05 15:11:05 +0800

    feat(project): Added styling through inline and stylesheet

--- MyFirstReactNativeApp/App.js
+++ MyFirstReactNativeApp/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "#00f" }}>Hello World!</Text>
+      <TextInput style={styles.style1} placeholder="Input Text" />
+      <Button title="Submit" />
     </View>
   );
 }
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  style1: {
+    borderWidth: 2,
+    borderColor: "black",
+    padding: 8,
   },
 });

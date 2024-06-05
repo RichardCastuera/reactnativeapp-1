de0c920ad6fb3fc076cbe4212b1a6be12dc44d1a
Richard <richardcastuera16@gmail.com>
2024-06-05 15:40:52 +0800

    feat(project): Used flexbox for layout

--- MyFirstReactNativeApp/App.js
+++ MyFirstReactNativeApp/App.js
@@ -2,24 +2,42 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "#00f" }}>Hello World!</Text>
-      <TextInput style={styles.style1} placeholder="Input Text" />
-      <Button title="Submit" />
+    <View style={styles.appContainer}>
+      <View style={styles.userGoals}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button style={styles.styleButton} title="Add Goal" />
+      </View>
+      <View style={styles.userAddedGoals}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 50,
   },
-  style1: {
-    borderWidth: 2,
-    borderColor: "black",
+  userGoals: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccccc",
+    flex: 1,
     padding: 8,
+  },
+  styleButton: {
+    padding: 8,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green",
+    color: "green",
+  },
+  userAddedGoals: {
+    marginVertical: 20,
   },
 });
